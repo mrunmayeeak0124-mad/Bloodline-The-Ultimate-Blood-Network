d599d157a5d8faba7f371f036ed5ed9de5744df1
Mrunmayee <mrunmayeejk0124@gmail.com>
2026-07-23 23:54:12 +0530

    Updated backend API endpoint to Vercel production server

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 // --- CENTRAL BLOODLINE NETWORK GATEWAY ---
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://hemoflow-iic27l3x3-mrunmayee2409.vercel.app';
 
 const BloodLineAPI = {
   
